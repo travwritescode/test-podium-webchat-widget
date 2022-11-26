7f144b89241541d4f616e0c322af4d89b4e3490e
Travis <travis.anderson30@gmail.com>
2022-11-25 18:28:26 -0700

    Fixed conftest after an experiment

--- conftest.py
+++ conftest.py
@@ -1,18 +1,12 @@
 import pytest
 from selenium import webdriver
 
-@pytest.fixture(params=['chrome', 'firefox'], scope='function')
+@pytest.fixture(scope='function')
 def browser():
     chromeOptions = webdriver.ChromeOptions()
     chromeOptions.headless = True
     chromeOptions.add_argument("--log-level=3")
     browser = webdriver.Chrome(options=chromeOptions)
-
-    # Below code for Firefox testing
-    # firefoxOptions = webdriver.FirefoxOptions()
-    # firefoxOptions.headless = True
-    # firefoxOptions.add_argument("--log-level=3")
-    # browser = webdriver.Firefox()
 
     browser.get("https://demo.podium.tools/qa-webchat-lorw/")
 
